370ab8e12ab09880bfc9083688a5478d0746505a
pedropanosyan <pedro.panosyan@ing.austral.edu.ar>
2024-06-02 17:34:43 -0300

    Starting WIP

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,36 @@
 import NavBar from "@/components/navbar/navbar";
 import WalletConnect from "@/components/walletConnect/WalletConnect";
 import {ProjectCard} from "@/components/projectCard/projectCard";
+import Button from "@/components/button/button";
 const image = "https://s3-alpha-sig.figma.com/img/9f59/ce21/c74ad96c0d1a61ac9fa97f6caef4341c?Expires=1716768000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Yei1q7fbh8SiMNZSKFg53FuoxRBPlS2JQ8FRORX17f8wO4pfP3ykn9MtSpl7HcsagUijwckyNxywwglxMPxzhq9iZ1MRHGVHA0oM~bCxbptinX7HRSPXwb8iJGC~hFnB4bvc25IHw10cR~YSstl1qWKZ-SljxordAmfXNM8TSmLY6-IUOZv1wPmZq08-4~v2WeX1dMQ4lc5M73C1vU~ClGP06djwTIeURLtkRwOjB1PGzhRbRH231XL-huIhBpsG3u2Rk3~LAzflBRMOep8wq3y-MB-S-WsG~s6zylgNVw2KvCpVXGXizUnB3LIsLLTGKyh6f5M6DXHP3~1rJ5rdKQ__"
 
 export default function Home() {
+
+    const handleConnectCustodialWallet = () => {
+        const data = {
+            "address": "0x2c7cbe8911a49aa202cf2814732b510e8f25f732",
+            "companyId": "6579c1d289ad6a000767524f",
+            "coin": "WLD",
+            "network": "OPTIMISM",
+            "ramp": "on",
+            "userId": "238301",
+            "fiatAmount": "30000",
+            "cryptoAmount": "",
+            "successRedirectUrl": "",
+            "externalId": ""
+        }
+        fetch(
+            'https://ramp-qa.manteca.dev/auth/w-signup/id',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            }
+        );
+    }
+
   return (
     <>
       {/* TEMPORARY */}
@@ -21,6 +48,9 @@ export default function Home() {
                     raisedAmount="500.000"
               />
           </div>
+          <Button onClick={handleConnectCustodialWallet}>
+              Invertir por bancos
+          </Button>
       </main>
     </>
   );
